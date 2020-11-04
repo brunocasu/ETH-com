bd37981c109a202e3eafb07b8df5cda90b8e9910
Bruno Casu <brunocasu8@gmail.com>
2020-11-04 12:20:04 -0300

    adding gitignore

--- CM7/Debug/Drivers/BSP/Components/lan8742.d
+++
@@ -1,5 +0,0 @@
-Drivers/BSP/Components/lan8742.o: \
- /home/brunocasu/STM32CubeIDE/workspace_1.4.0/ETH-com/Drivers/BSP/Components/lan8742/lan8742.c \
- /home/brunocasu/STM32CubeIDE/workspace_1.4.0/ETH-com/Drivers/BSP/Components/lan8742/lan8742.h
-
-/home/brunocasu/STM32CubeIDE/workspace_1.4.0/ETH-com/Drivers/BSP/Components/lan8742/lan8742.h:
